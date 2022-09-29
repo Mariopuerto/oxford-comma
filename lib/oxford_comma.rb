f077b709e958004bf13c9db5f3cad1d55698aeb3
“mariopuerto” <mariopuerto.nyc@gmail.com>
2022-09-29 17:40:38 -0400

    commit tree dbf3c7fe46d84722f5970955c11496ff1e44d953

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        array.each.collect do |value|
+            if value == array[-1]
+                array[-1] = "and " << value
+            end
+        end
+        array.join(", ")
+    end
 end
+
+# returns a string without any additional formatting when given a 1-element array (FAILED - 1)
+# adds 'and' between elements when given a 2-element array (FAILED - 2)
+# adds commas plus a final 'and' when given a 3-element array (FAILED - 3)
+# correctly formats arrays of lengths greater than three (FAILED - 4)
+
+ 
